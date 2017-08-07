ce5a1cc7317f7f4a1d2a44f9d4a6c9f32f63fd89
Xue Qi <690139760@qq.com>
2017-08-07 14:16:32 +0800

    101

--- tree/medium/101/test101.cpp
+++ tree/medium/101/test101.cpp
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <iostream>
+#define LEN (sizeof(TreeNode))
 using namespace std;
 struct TreeNode {
 	int val;
@@ -6,10 +8,20 @@ struct TreeNode {
 	TreeNode *right;
 	TreeNode(int x) :val(x), left(NULL), right(NULL) {}
 };
+void CreatTree(TreeNode *root, int count, TreeNode *current);
+void PrintTree(TreeNode *root);
 bool isMirror(TreeNode *root, TreeNode *root1);
 void main() {
 	TreeNode *root;
-	isMirror(root, root);
+	TreeNode *current;
+	int count = 0;
+	root = (TreeNode*)malloc(LEN);
+	current = root;//记住一定要先给root申请内存，并且要给current初始化
+	CreatTree(root,count,current);
+	root = current;
+	PrintTree(current);
+	bool result = isMirror(root->left, root->right);
+	cout << "result:" << result;
 }
 bool isMirror(TreeNode *root, TreeNode *root1) {
 	if (root == NULL && root1 == NULL)
@@ -18,5 +30,38 @@ bool isMirror(TreeNode *root, TreeNode *root1) {
 		return false;
 	else if (root != NULL && root1 == NULL)
 		return false;
-	return((root->val == root1->val) && (isMirror(root->left,root1->right) && (root->right,root1->left)));
+	//return((root->val == root1->val) && (isMirror(root->left,root1->right) && (root->right,root1->left)));
+	if (root->val == root1->val) {
+		bool isSame1 = isMirror(root->left,root1->right);
+		bool isSame2 = isMirror(root->right,root1->left);
+		return isSame1&&isSame2;
+	}
+	return false;
+}
+void CreatTree(TreeNode *root, int count, TreeNode *current) {
+	int temp;
+	count++;
+	if (count == 1)
+		current = root;
+		cin >> temp;
+		if (temp == 0) {
+			root->val = NULL;
+			root->left = root->right = NULL;
+		}
+		else {
+			root->val = temp;
+			root->left = (TreeNode*)malloc(LEN);
+			CreatTree(root->left,count,current);
+			root->right = (TreeNode*)malloc(LEN);
+			CreatTree(root->right,count,current);
+	}
+}
+void PrintTree(TreeNode *root) {
+	if (root->val == NULL)
+		cout << "0";
+	else if (root != NULL) {
+		cout << root->val;
+		PrintTree(root->left);
+		PrintTree(root->right);
+	}
 }
